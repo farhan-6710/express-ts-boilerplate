629116c63c7545af51e88e9b6bf24327985ba6b8
Farhan-6710 <itz.me.farhan6710@gmail.com>
2026-02-28 07:36:51 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+//src/app.ts
+
+import express from 'express';
+import itemRoutes from './routes/itemRoutes';
+import { errorHandler } from './middlewares/errorHandler';
+
+const app = express();
+
+app.use(express.json());
+
+// Routes
+app.use('/api/items', itemRoutes);
+
+// Global error handler (should be after routes)
+app.use(errorHandler);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+//src/server.ts
+
+import app from './app';
+import config from './config/config';
+
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
